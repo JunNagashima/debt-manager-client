1cfa0e8781838f2325ee2b791f7c59bcdc45c0c1
JunNagashima <j.nagashima.carecon@gmail.com>
2025-11-24 22:00:44 +0900

    [fix] scssファイルのインデント修正

---
+++ app/dashboard/_components/DashBoardPage/DashBoardPage.tsx
@@ -0,0 +1,9 @@
+const DashBoardPage = () => {
+  return (
+    <div>
+      Enter
+    </div>
+  );
+}
+
+export default DashBoardPage;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import styles from './page.module.scss';
+import DashBoardPage from './_components/DashBoardPage/DashBoardPage';
+
 const page = () => {
   return (
-    <div>
-      Enter
+    <div className={styles.dashboardContainer}>
+      <p className={styles.dashboardTitle}>残高サマリー</p>
+      <div className={styles.border} />
+      <DashBoardPage />
     </div>
   );
 }
